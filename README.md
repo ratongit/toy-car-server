a0b3f2d8bf5d27c4a455f3fe8f7d355c7dac2d96
sabboshachi546 <mohantosabbosachi@gmail.com>
2023-05-22 23:19:22 +0600

    all problem solved

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Toy-car-server
 # Toy-car-server
 # b7a11-toy-marketplace-server-side-Raton-Kumar-1122
+## githab link https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-server-side-Raton-Kumar-1122
--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
 
 
     app.get('/alltoy', async (req, res) => {
